a2cb384a9fe1d1862ebb87ffa53e006f6bfb62ce
chensy <chensy@gmail.com>
2023-06-15 15:14:20 +0800

    first commit

---
+++ src/test/java/com/xmut/springbootdemo03/SpringbootDemo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmut.springbootdemo03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
